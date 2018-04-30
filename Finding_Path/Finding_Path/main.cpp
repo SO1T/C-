92bd514a718e1ea48c91cd7e2592466c88f39464
Denys Pravdyvyj <30390172+SO1T@users.noreply.github.com>
2018-04-30 18:34:15 +0300

    Add files via upload
    Implemented A* 
    Added priority queue

--- Finding_Path/Finding_Path/main.cpp
+++ Finding_Path/Finding_Path/main.cpp
@@ -1,26 +1,32 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <queue>
 
 using namespace std;
 
 struct point { int x, y; };
 
+struct item
+{
+	point data;
+	int priority;
+};
+
 struct Queue
 {
 	int front, rear, size;
 	unsigned capacity;
-	point *array;
+	item *array;
 };
 
 struct Queue* createQueue(unsigned capacity)
 {
-	Queue *queue = (Queue*) malloc(sizeof(Queue));
+	Queue *queue = (Queue*)malloc(sizeof(Queue));
 	queue->capacity = capacity;
-	queue->front = 0;
-	queue->size = 0;
+	queue->front = queue->size = 0;
 	queue->rear = capacity - 1;
-	queue->array = (point*) malloc(queue->capacity * sizeof(point));
+	queue->array = (item*)malloc(queue->capacity * sizeof(item));
 	return queue;
 }
 
@@ -34,37 +40,55 @@ bool isFull(Queue *queue)
 	return queue->size == queue->capacity;
 }
 
-void enqueue(Queue *queue, point item)
+void enqueue(Queue *queue, point d, int pr)
 {
 	if (isFull(queue))
 		return;
 	queue->rear = (queue->rear + 1) % queue->capacity;
-	queue->array[queue->rear] = item;
-	queue->size += 1;
+	queue->array[queue->rear].data = d;
+	queue->array[queue->rear].priority = pr;
+	queue->size = queue->size + 1;
+
+	int s = queue->size;
+	int i, j, key;
+	item temp;
+
+	for (i = 1; i < s; i++)
+	{
+		key = queue->array[i].priority;
+		temp = queue->array[i];
+		j = i - 1;
+		while (j >= 0 && queue->array[j].priority < key)
+		{
+			queue->array[j + 1] = queue->array[j];
+			j = j - 1;
+		}
+		queue->array[j + 1] = temp;
+	}
 }
 
 point dequeue(Queue *queue)
 {
 	if (isEmpty(queue))
-		return { INT_MIN, INT_MIN };
-	point item = queue->array[queue->front];
+		return { INT_MIN,INT_MIN };
+	point data = queue->array[queue->front].data;
 	queue->front = (queue->front + 1) % queue->capacity;
 	queue->size -= 1;
-	return item;
+	return data;
 }
 
 point front(Queue *queue)
 {
 	if (isEmpty(queue))
 		return { INT_MIN, INT_MIN };
-	return queue->array[queue->front];
+	return queue->array[queue->front].data;
 }
 
 point rear(Queue *queue)
 {
 	if (isEmpty(queue))
 		return { INT_MIN, INT_MIN };
-	return queue->array[queue->rear];
+	return queue->array[queue->rear].data;
 }
 
 string **create_grid(string str)
@@ -102,40 +126,50 @@ string **create_grid(string str)
 	return grid;
 }
 
-void reconstruct(string **grid, int **dir, point start, point finish, int last)
+int heuristic(point c, point f)
+{
+	int dx = abs(c.x - f.x);
+	int dy = abs(c.y - f.y);
+
+	return dx + dy;
+}
+
+void reconstruction(string **grid, int m, int n, point start, point finish, int **dir)
 {
 	int di[4] = { -1,0,1,0 };
 	int dj[4] = { 0,1,0,-1 };
 	int **d = dir;
 	Queue *queue = createQueue(100);
-	enqueue(queue, finish);
-	int l = last;
+	enqueue(queue, finish, 0);
 	int ch = 66;
 
 	while (!isEmpty(queue))
 	{
 		point p = dequeue(queue);
+		if (heuristic(p, start) == 0)
+			break;
 		for (int k(0); k < 4; k++)
 		{
 			point newp = { p.x + di[k],p.y + dj[k] };
-			if (dir[newp.x][newp.y] == l - 1 || l == 0)
+			if (dir[newp.x][newp.y] != 0)
 			{
 				grid[newp.x][newp.y] = (char)ch;
-				enqueue(queue, newp);
-				l--;
+				enqueue(queue, newp, heuristic(newp, start));
 				ch++;
 			}
 		}
 	}
 	grid[finish.x][finish.y] = (char)65;
-	for (int f(0); f < 8; f++)
-	{
-		for (int g(0); g < 8; g++)
-			cout << grid[f][g];
-		cout << endl;
-	}
-}
 
+	ofstream fout("exit.txt");
+	for (int f(0); f < m; f++)
+	{
+		for (int g(0); g < n; g++)
+			fout << grid[f][g];
+		fout << endl;
+	}
+	fout.clear();
+}
 
 void Djicstra(string **grid, int m, int n, point start, point finish)
 {
@@ -153,11 +187,16 @@ void Djicstra(string **grid, int m, int n, point start, point finish)
 	d[start.x][start.y] = 1;
 
 	Queue *queue = createQueue(m * n);
-	enqueue(queue, start);
+	enqueue(queue, start, heuristic(start, finish));
+
+	point p;
 
 	while (!isEmpty(queue))
 	{
-		point p = dequeue(queue);
+		p = dequeue(queue);
+		if (heuristic(p, finish) == 0)
+			break;
+		//cout << p.x << " - x:y - " << p.y << " d - " << heuristic(p, finish) << endl;
 		for (int k(0); k < 4; k++)
 		{
 			point newp = { p.x + di[k],p.y + dj[k] };
@@ -165,27 +204,19 @@ void Djicstra(string **grid, int m, int n, point start, point finish)
 				if (grid[newp.x][newp.y] == " " && d[newp.x][newp.y] == 0)
 				{
 					d[newp.x][newp.y] = d[p.x][p.y] + 1;
-					enqueue(queue, newp);
+					enqueue(queue, newp, d[newp.x][newp.y] + heuristic(newp, finish));
 				}
 		}
 	}
-	reconstruct(grid, d, start, finish, d[finish.x][finish.y]);
-}
 
+	reconstruction(grid, m, n, start, finish, d);
+
+}
 
 int main()
 {
 	string **grid = create_grid("C:\\Users\\den27\\Desktop\\labirint.txt");
-	Djicstra(grid, 8, 8, { 6,1 }, { 1,6 });
-	grid[6][1] = "a";
-	grid[1][6] = "a";
-
-	/*for (int i(0); i < 8; i++)
-	{
-		for (int j(0); j < 8; j++)
-			cout << grid[i][j];
-		cout << endl;
-	}*/
+	Djicstra(grid, 8, 8, { 6,1 }, { 4,1 });
 
 	system("pause");
 	return 0;
